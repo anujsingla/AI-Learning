402125b1a19ce2b299f2a9a802448d4ca1d2ac0b
anujsingla.singla387@gmail.com <asingla@redhat.com>
2024-09-14 14:39:58 +0530

    Naive Bayes Algorithm .

--- NaiveBayesalgorithm.py
+++ NaiveBayesalgorithm.py
@@ -1,3 +1,4 @@
+# https://www.kaggle.com/code/prashant111/naive-bayes-classifier-in-python/notebook
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # import matplotlib.pyplot as plt # for data visualization purposes
@@ -7,16 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import RobustScaler
 import category_encoders as ce
-# %matplotlib inline
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-# import os
-# for dirname, _, filenames in os.walk('/dataset'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
-        
 
 data = '<file path>'
 df = pd.read_csv(data, header=None, sep=',')
